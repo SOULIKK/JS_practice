37c19d25c66898a52e6c1cc84d7f3501ecc7dd79
soulikk <enclumek23@naver.com>
2020-03-29 18:06:16 +0900

    3029_2

--- number_baseball/number_baseball.js
+++ number_baseball/number_baseball.js
@@ -1,52 +1,86 @@
 var num_content = [1,2,3,4,5,6,7,8,9];
-
 var num_array = [];
 
 for(var i = 0; i < 4; i++) {
-    var picked = num_content.splice(Mathl.ceil(Math.random() * (9 - i), 1)[0]);
+    var picked = num_content.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
     num_array.push(picked);
 }
+fail_cnt = 0;
 
 console.log(num_array);
+console.log(num_array.join(''));
 
 var body = document.body;
 
-var result = documen.createElement('h1');
-document.body.append(result);
+var result = document.createElement('h1');
+body.append(result);
 
 var form = document.createElement('form');
-document.body.append(form);
+body.append(form);
 
 var input_form = document.createElement('input');
-input_form.type = text;
+form.append(input_form);
+input_form.type = 'text';
 input_form.maxLength = 4;
 
 var btn = document.createElement('button');
-btn.textContent = '입력';
 form.append(btn);
+btn.textContent = "입력";
 
-form.addEventListener('submit', function cbf(e) {
+var fail_cnt = 0;
+
+form.addEventListener('submit', function 비동기(e) {    // 엔터를 쳤을 때
     e.preventDefault();
     var answer = input_form.value;
-    if (answer === num_array.join('')) {
+    console.log(answer);
+    if (answer === num_array.join('')) {    // 답이 맞으면
         result.textContent = '홈런';
-        input_form.value = '';
+        input_form = '';
         input_form.focus();
+
         num_content = [1,2,3,4,5,6,7,8,9];
         num_array = [];
         for (var i = 0; i < 4; i++) {
-            var pickded = num_content.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+            var picked = num_content.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+            num_array.push(picked);
         }
-    } else {
+        fail_cnt = 0;
 
+    } else {    // 답이 틀리면
+        var answer_array = answer.split('');
+        var strike = 0;
+        var ball = 0;
+        fail_cnt += 1;
+        if (fail_cnt > 10) {
+            result.textContent = '10번 넘게 틀려서 실패! 정답 : ' + num_array.join(',');
+        }
+        console.log('답이 틀리면');
+
+        for (var i = 0; i < 3; i ++) {
+            if (Number(answer_array[i]) === num_array[i]) { // 같은 자리인지 확인
+                console.log('같은 자리?');
+                strike += 1;
+            } else if (num_array.indexOf(Number(answer_array[i]) > -1)) {  // 같은 자리는 아니지만 숫자가 있는지 확인
+                ball += 1;
+                console.log('겹치는 숫자?');
+            }
+        }
+        result.textContent = strike + ' 스트라이트 ' + ball + ' 볼 입니다';
+        input_form.value = '';
+        input_form.focus();
     }
 });
+
+
+
 
 // push: 마지막에 추가
 // pop: 마지막것 뽑기
 // unshift: 처음에 추가
-// shif: 처음것 뽑기
+// shift: 처음것 뽑기
 // splice(위치, 갯수): 위치로부터 갯수만큼 배열에서 뽑음
 
 // 문자.split(구분자) -> 배열
 // 배열.join(구분자) -> 문자
+
+// 배열.indexOf(값): 값의 위치, 없으면 -1
