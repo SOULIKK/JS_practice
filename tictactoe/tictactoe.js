82a791117e6f2472af397be8a67d9387445e7d76
soulikk <enclumek23@naver.com>
2020-04-12 17:25:04 +0900

    0412

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -3,48 +3,74 @@ var 테이블 = document.createElement('table');
 var 줄들 = [];
 var 칸들 = [];
 var 턴 = 'X';
+var 결과 = document.createElement('div');
 
 var 비동기콜백 = function(이벤트) {
-    console.log(이벤트.target);
-    console.log(이벤트.target.parentNode);  // 줄
-    console.log(이벤트.target.parentNode.parentNode);   // 테이블
+    // console.log(이벤트.target);
+    // console.log(이벤트.target.parentNode);  // 줄
+    // console.log(이벤트.target.parentNode.parentNode);   // 테이블
 
     var 몇줄 = 줄들.indexOf(이벤트.target.parentNode);
-    console.log('몇줄', 몇줄);
+    // console.log('몇줄', 몇줄);
     var 몇칸 = 칸들[몇줄].indexOf(이벤트.target);
-    console.log('몇칸', 몇칸);
+    // console.log('몇칸', 몇칸);
 
     if (칸들[몇줄][몇칸].textContent !== '') {   // 칸이 이미 채워져 있는가?
         console.log('빈칸아님');
     } else {
         console.log('빈칸임');
         칸들[몇줄][몇칸].textContent = 턴;
-        if (턴 === 'X') {
-            턴 = 'O';
-        } else {
-            턴 = 'X';
-        }
-    }
-    // 세칸 다 채워졌나?
-    var 다참 = false;
-    // 가로줄 검사
-    if (칸들[몇줄][0].textContent === 턴 && 칸들[몇줄][1].textContent === 턴 && 칸들[몇줄][2].textContent === 턴) {
-        다참 = true;
-    }
-    // 세로줄 검사
-    if (칸들[0][몇칸].textContent === 턴 && 칸들[1][몇칸].textContent === 턴 && 칸들[2][몇칸].textContent === 턴) {
-        다참 = true;
-    }
-    // 대각선 검사
-    if (몇줄 === 몇칸) {  // 대각선 검사 필요한 경우
-        if (칸들[0][2].textContent === 턴 && 칸들[1][1].textContent === 턴 && 칸들[2][0].textContent === 턴) {
+
+        // 세칸 다 채워졌나?
+        var 다참 = false;
+        // 가로줄 검사
+        if (
+            칸들[몇줄][0].textContent === 턴 && 
+            칸들[몇줄][1].textContent === 턴 && 
+            칸들[몇줄][2].textContent === 턴
+        ) {
             다참 = true;
         }
-    }
-
-    // 다찼으면
-    if (다참) {
-        console.log(턴 + '님이 승리!');
+        // 세로줄 검사
+        if (
+            칸들[0][몇칸].textContent === 턴 && 
+            칸들[1][몇칸].textContent === 턴 && 
+            칸들[2][몇칸].textContent === 턴
+        ) {
+            다참 = true;
+        }
+        // 대각선 검사
+        if (
+            칸들[0][0].textContent === 턴 &&
+            칸들[1][1].textContent === 턴 &&
+            칸들[2][2].textContent === 턴
+        ) {
+            다참 = true;
+        }
+        if (
+            칸들[0][2].textContent === 턴 &&
+            칸들[1][1].textContent === 턴 &&
+            칸들[2][0].textContent === 턴
+        ) {
+            다참 = true;
+        }
+  
+        // 다찼으면
+        if (다참) {
+            결과.textContent = 턴 + ' 님이 승리';
+            턴 = 'X';
+            칸들.forEach(function (줄) {
+                줄.forEach(function (칸) {
+                    칸.textContent = '';
+                });
+            });
+        } else {  // 다 안찼으면
+            if (턴 === 'X') {
+                턴 = 'O';
+            } else {
+                턴 = 'X';
+            }
+        }
     }
 };
 
@@ -61,7 +87,15 @@ for (var i = 1; i <= 3; i += 1) {
     테이블.appendChild(줄);
 }
 바디.appendChild(테이블);
-console.log('줄들', 줄들, '칸들', 칸들);
+바디.appendChild(결과);
+
+
+
 
 // e.target === 클릭된 애
 // e.target.parentNode === 클릭된 애 부모태그
+
+// input의 값 : value
+// 태그 안 글자는 textContent
+
+// Math.abs() : 절대값
